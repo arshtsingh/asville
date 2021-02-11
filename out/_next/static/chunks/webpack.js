cb9e0ed78081301690192e1de6e92a3819e06ad3
arsh <>
2021-02-10 18:45:35 -0600

    updated package lock

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7228bc374804361be32f";
+/******/ 	var hotCurrentHash = "83c024aa5d50f61ae8c8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
